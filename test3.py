246ad2a3778a076c0b076c68a05a590ee558acec
Mathieu GRUSON <mathieu.gruson@gmail.com>
2023-02-26 15:14:16 +0100

    amount and theme

--- test3.py
+++ test3.py
@@ -34,10 +34,10 @@ s_tags = [s for s in soup.find_all('span', {'class': 'tag'})]
 with open('output.csv', mode='w') as output_file:
     output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    output_writer.writerow(['amount', 'theme'])
+    output_writer.writerow(['theme', 'montant'])
 
     # Iterate through pairs of p_tags and s_tags.
     for p, s in zip(p_tags, s_tags):
-        output_writer.writerow([p.get_text(), s.get_text()])
+        output_writer.writerow([s.get_text(), p.get_text()])
 
 # Close the brow
